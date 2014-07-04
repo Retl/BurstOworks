0a0f9a2c48b90ad20c9d02777b91afdbc6e444f3
Retl <carlis.moore@gmail.com>
2014-07-04 14:53:55 -0400

    Mouse Movement Works
    A jump-to-location-of-rightclick has been implemented.

--- main.lua
+++ main.lua
@@ -15,11 +15,26 @@ function love.load()
 	}
 	
 	function char1.draw()
-		love.graphics.circle( "fill", 300, 300, 32, 100 )
+		love.graphics.circle( "fill", char1.x, char1.y, 32, 100 )
+	end
+	
+	function char1.jumpToMouse()
+		char1.x = love.mouse.getX()
+		char1.y = love.mouse.getY()
 	end
 end
 
 function love.update(dt)
+	--Handling Mouse Input
+	if love.mouse.isDown("r") then  --Left Mouse Button
+		
+	end
+	
+	if love.mouse.isDown("r") then  --Right Mouse Button
+		char1.jumpToMouse()
+	end
+	
+	-- Handling Keyboard Input
    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
       x = x + (speed * dt)
    end
