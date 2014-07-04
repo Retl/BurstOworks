b0cbc30f0755657f1e078e7ad3b104fd85d803ae
Retl <carlis.moore@gmail.com>
2014-07-04 15:47:59 -0400

    Player Size Grows While Mouse is Down

--- main.lua
+++ main.lua
@@ -8,31 +8,59 @@ function love.load()
 	
 	speed = 300
 	
+	mouseDownPrevious = false;
+	mouseChanged = false;
+	
 	char1 = {
 		name = "Big O",
 		x = maxx / 2,
-		y = maxy / 2
+		y = maxy / 2,
+		minsize = 16,
+		size = 16,
 	}
 	
 	function char1.draw()
-		love.graphics.circle( "fill", char1.x, char1.y, 32, 100 )
+		love.graphics.circle( "fill", char1.x, char1.y, char1.size, 100 )
 	end
 	
 	function char1.jumpToMouse()
 		char1.x = love.mouse.getX()
 		char1.y = love.mouse.getY()
 	end
+	
+	function char1.resetSize()
+		char1.size = char1.minsize
+	end
+	
+	function char1.grow(rate)
+		char1.size = char1.size + 10 * rate
+	end
 end
 
 function love.update(dt)
 	--Handling Mouse Input
-	if love.mouse.isDown("r") then  --Left Mouse Button
-		
+	
+	if MouseDownPrevious ~= love.mouse.isDown("l") then
+		mouseChanged = true
+	else
+		mouseChanged = false
+	end
+	
+	--If the state of the mouse being down is different than before, do something different.
+	
+	if love.mouse.isDown("l") then  --Left Mouse Button
+		--Do a thing. Maybe create a bubblethingy.
 	end
 	
 	if love.mouse.isDown("r") then  --Right Mouse Button
 		char1.jumpToMouse()
+		char1.grow(dt)
+	else
+		char1.resetSize()
 	end
+	
+	-- Update the copy of  the mouse's click state.
+	MouseDownPrevious = love.mouse.isDown("l")
 	
 	-- Handling Keyboard Input
    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
